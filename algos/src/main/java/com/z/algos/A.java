547466849c52bdf215e968b7fad29d32b95e5ff8
Zach Bolin <zdbolin@gmail.com>
2022-07-06 21:40:40 -0500

    Updated z

---
+++ algos/src/main/java/com/z/algos/A.java
@@ -0,0 +1,5 @@
+package com.z.algos;
+
+public  class A {
+
+}
---
+++ algos/src/test/java/com/z/algos/ATest.java
@@ -0,0 +1,21 @@
+package com.z.algos;
+
+
+import org.junit.Test;
+
+import junit.framework.Assert;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Before;
+
+public class ATest {
+
+
+
+    @Test
+    public void a_test() {
+        Assert.assertTrue(true);
+    }
+
+}
