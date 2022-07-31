c1e242eff7389e22512cae39825d5856804cb5b3
Zach Bolin <zdbolin@gmail.com>
2022-07-31 14:17:13 -0500

    Updated z

--- aws/tags/README.md
+++ aws/tags/README.md
@@ -1,3 +1,9 @@
 # Using AWS Tags and Resource Groups
 
-" list instances
+SearchInstances Mod
+CreateImage i-#### Base
+ListSg
+ListSubnet
+CreateEc2 ami-0e2266fc70bfa1032 t3.micro sg-0ad2701df316ff423 subnet-06a36b484cb29af8e
+CreateTag i-#### Name "Test Web Server"
+
--- aws/tags/go.zsh
+++ aws/tags/go.zsh
@@ -1,20 +1,3 @@
 Go() {
     local image_id = ""
 }
-GetEc2InstanceIds() {
-
-}
-CreateImage() {
-    local instance_id = $1
-    local name = $2
-
-}
-CrateEc2() {
-    local image_id = $1 # ami-xxxxxxxxx
-    local key_name = $2 # MyKeyPair
-    local security_group = $3 # sg-xxxxxx
-    local subnet_id = $4 # subnet-xxxxxx
-    local user_data = $5
-    aws ec2 run-instances --image-id ami-xxxxxxxx --count 1 --instance-type t2.micro --key-name MyKeyPair --security-group-ids sg-903004f8 --subnet-id subnet-6e7f829e
-
-}
